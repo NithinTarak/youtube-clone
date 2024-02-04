3e548356b0a38de18bc07ffb9e7d74b16f7cabd6
Nithin <nithinv365@gmail.com>
2024-02-04 11:46:20 +0530

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
+
+const options = {
+  url: BASE_URL,
+  params: {
+    part: 'snippet',
+    videoId: 'M7FIvfx5J10'
+  },
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
+const fetchFromAPI = async(url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`,options);
+    return data;
+
+}
+export default fetchFromAPI;
